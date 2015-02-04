8a18f756d8e78e251c888fd88a113ddfa66019ac
NegiXNodoka <miyaha77@yahoo.com>
2015-02-04 20:29:01 +0800

    add/edit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: true
+  validates :stock, presence: true, numericality: true
+
+  before_validation :load_defaults
+
+  def load_defaults
+  	if self.new_record?
+  	  self.status = "pending"
+    end 
+  end
 end
